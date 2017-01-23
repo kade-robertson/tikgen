b442640d1f97b843656c16bff0adc76841141fb9
kade-robertson <kade.robertson96@gmail.com>
2017-01-23 15:53:29 -0500

    cleaned up with array copying

--- tikgen/Program.cs
+++ tikgen/Program.cs
@@ -14,30 +14,23 @@ namespace tikgen
             if (args.Length == 2) {
                 Console.WriteLine(string.Format("[i] Obtaining TMD for Title ID {0}...", args[0]));
                 byte[] tmddata = null;
-
                 using (var wc = new WebClient()) {
                     tmddata = wc.DownloadData(string.Format("http://ccs.cdn.c.shop.nintendowifi.net/ccs/download/{0}/tmd", args[0]));
                 }
-
                 if (tmddata == null) {
                     Console.WriteLine("[!] Unable to obtain TMD. Exiting.");
                     return;
                 }
-
                 Console.WriteLine("[i] Getting version data...");
-
                 var type = tmddata[0x3];
-                var ver1 = tmddata[offsets[type] + 0x9C];
-                var ver2 = tmddata[offsets[type] + 0x9D];
+                var vers = new byte[2];
+                Array.Copy(tmddata, offsets[type] + 0x9C, vers, 0, 2);
+
 
                 Console.WriteLine("[i] Building ticket data...");
-
-                var outfile = build_ticket(ver1, ver2, args[0], args[1]);
-
+                var outfile = build_ticket(vers, args[0], args[1]);
                 Console.WriteLine(string.Format("[i] Writing to {0}.tik...", args[0]));
-
                 File.WriteAllBytes(string.Format("{0}.tik", args[0]), outfile);
-
                 Console.WriteLine("[i] Ticket generated successfully!");
             } else {
                 Console.WriteLine("[!] Usage: tikgen.exe <Title ID> <Encrypted Title Key>");
@@ -45,7 +38,7 @@ namespace tikgen
             }
         }
 
-        static byte[] build_ticket(byte ver1, byte ver2, string titleid, string enckey) {
+        static byte[] build_ticket(byte[] vers, string titleid, string enckey) {
             var temp = Base.tikbase;
             var size = 0x140;
             var b_titleid = new byte[8];
@@ -58,16 +51,9 @@ namespace tikgen
                 b_enckey[i / 2] = Convert.ToByte(enckey.Substring(i, 2), 16);
             }
 
-            for (int i = 0; i < 8; i++) {
-                temp[size + 0x9C + i] = b_titleid[i];
-            }
-
-            temp[size + 0xA6] = ver1;
-            temp[size + 0xA7] = ver2;
-
-            for (int i = 0; i < 16; i++) {
-                temp[size + 0x7F + i] = b_enckey[i];
-            }
+            Array.Copy(b_titleid, 0, temp, size + 0x9C, 8);
+            Array.Copy(vers, 0, temp, size + 0xA6, 2);
+            Array.Copy(b_enckey, 0, temp, size + 0x7F, 16);
 
             return temp;
         }
